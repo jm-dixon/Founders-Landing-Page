6ef45f23a55d90f36bc112244ee33e817ab92b76
ryantcarter <ryn@crtr.co.uk>
2022-09-29 17:06:05 +0100

    Tweak Overlay Colour in Hero

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -95,7 +95,7 @@ export default {
     z-index: 1;
     width: 100%;
     height: 100%;
-    background: var(--color-primary);
+    background: var(--color-blue);
     mix-blend-mode: multiply;
     opacity: 0.55;
   }
